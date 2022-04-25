36dcd618228f2ea3595c666fa1f5fb08b5c4957c
aximum13 <aximum2@yandex.ru>
2022-04-25 16:58:52 +0300

    dise.rb v.2

--- operators/dice.rb
+++ operators/dice.rb
@@ -1,7 +1,15 @@
+def show_dice(num)
+  num.times do
+    sleep 0.01
+    print "#{rand(6) + 1}\r"
+  end
+end
+
 puts "Сколько кинуть кубиков?"
 
 number = gets.to_i
 
-number.times do 
-  puts rand(6) + 1
+number.times do
+  show_dice(100)
+  puts (rand(6) + 1)
 end
