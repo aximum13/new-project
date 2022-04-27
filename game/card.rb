a055a0a16b16e331d85bd4e2a97a2d7d67d6e4d7
aximum <aximum2@yandex.ru>
2022-04-27 16:06:30 +0300

    all modify

--- game/card.rb
+++ game/card.rb
@@ -1,5 +1,5 @@
 values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Валет", "Дама", "Kороль", "Туз"]
  
-suits = ["Буби", "черви", "крести", "пики"]
+suits = ["♠", "♣", "♦", "♥"]
 
-puts "#{values.sample} #{suits.sample.capitalize}"
+puts "#{values.sample} #{suits.sample}"
