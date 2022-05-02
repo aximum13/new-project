18d774c32f5e2824d0705c009c70e43f151de2e3
aximum13 <aximum2@yandex.ru>
2022-05-02 17:13:29 +0300

    added a comment for the hbday.rb

--- time/hbday.rb
+++ time/hbday.rb
@@ -1,38 +1,38 @@
-require "date"
-require "petrovich"
+require "date" #Подключаем файл date для работы с классом Date
+require "petrovich" #Подключаем gem "petrovich" для работы со склонением имени, фамилии
 
-current_path = File.dirname(__FILE__)
+current_path = File.dirname(__FILE__) #задаём корректный путь для получения пути к папке текущей программы
 
-hbday_file = File.new(current_path + '/files/hbday.txt', 'r:UTF-8')
-hbday = hbday_file.readlines
-hbday_file.close
+hbday_file = File.new(current_path + "/files/hbday.txt", "r:UTF-8") # открываем файл для чтения
+hbday = hbday_file.readlines  # создаём один массив строк, разбитых по символам переноса
+hbday_file.close #закрываем файл
 
 for day in hbday
- p day.chomp!
+  day.chomp! # Удаляем символ переноса строки из каждого элемента, отрезая последний символ
 end
 
-time = Date.today
+time = Date.today #создаём новую переменную и записываем в неё текущую дату
 
-today = time.strftime("%d.%m")
-day3 = (Date.today + 3).strftime("%d.%m")
-week1 =(Date.today + 7).strftime("%d.%m") 
-week2 =(Date.today + 14).strftime("%d.%m")  
- 
-index = hbday.find_index {|i| i == today}
-index3 = hbday.find_index {|i| i == day3}
-index1 = hbday.find_index {|i| i == week1}
-index2 = hbday.find_index {|i| i == week2}
+today = time.strftime("%d.%m") #форматируем дату под день и месяц (07.07)
+day3 = (Date.today + 3).strftime("%d.%m") # создаём метки времени для будущего сравнения и также форматируем
+week1 = (Date.today + 7).strftime("%d.%m")
+week2 = (Date.today + 14).strftime("%d.%m")
 
-if hbday.index.include?(week2)
-  puts "Через 2 недели день рождения у #{Petrovich(firstname: hbday[index2+1], lastname:hbday[index2+2]).to(:genitive).to_s}"
+index = hbday.find_index { |i| i == today } # перебираем массив на совпадение данных: дата рождения (без года рождения) с сегодняшней датой и возвращаем индекс элемента, если он есть в массиве
+index3 = hbday.find_index { |i| i == day3 } # перебираем массив на совпадение данных: дата рождения (без года рождения) с датой за 3 дня до дня рождения и возвращаем индекс элемента, если он есть в массиве
+index1 = hbday.find_index { |i| i == week1 } # перебираем массив на совпадение данных: дата рождения (без года рождения) с датой за 1 неделю до дня рождения и возвращаем индекс элемента, если он есть в массиве
+index2 = hbday.find_index { |i| i == week2 } # перебираем массив на совпадение данных: дата рождения (без года рождения) с датой за 2 неделю до дня рождения и возвращаем индекс элемента, если он есть в массиве
+
+if hbday.include?(week2) #если данные "дата за 2 неделю до дня рождения" есть в массиве 
+  puts "Через 2 недели (#{week2}) день рождения у #{Petrovich(firstname: hbday[index2 + 1], lastname: hbday[index2 + 2]).to(:genitive).to_s}" #В качестве значений для аргументов класс Petrovich мы находим элемент по индексу, который обнаружили ранее (индекс элемента соответствует дате рождения из файла), следующей строкой (или элементом массива) является имя сотрудника, а после него - фамилия сотрудника. Поэтому для нахождения индекса элемента "Имени" необходимо к индексу элемента "Дата" прибавить 1, соответствено для нахождения индекса элемента "Имени" к индексу элемента "Дата" прибавить 2.
 end
-if hbday.index.include?(week1)
-  puts "Через неделю день рождения у #{Petrovich(firstname: hbday[index1+1], lastname:hbday[index1+2]).to(:genitive).to_s}"
+if hbday.include?(week1) #если данные "дата за 1 неделю до дня рождения" есть в массиве 
+  puts "Через неделю (#{week1}) день рождения у #{Petrovich(firstname: hbday[index1 + 1], lastname: hbday[index1 + 2]).to(:genitive).to_s}"
 end
-if hbday.index.include?(day3)
-  puts "Через 3 дня день рождения у #{Petrovich(firstname: hbday[index3+1], lastname:hbday[index3+2]).to(:genitive).to_s}"
+if hbday.include?(day3) #если данные "дата за 3 дня до дня рождения" есть в массиве 
+  puts "Через 3 дня (#{day3}) день рождения у #{Petrovich(firstname: hbday[index3 + 1], lastname: hbday[index3 + 2]).to(:genitive).to_s}"
 end
-if hbday.index.include?(today)
-  puts "Сегодня день рождения у #{Petrovich(firstname: hbday[index+1], lastname:hbday[index+2]).to(:genitive).to_s}"
+if hbday.include?(today) #если данные "сегодняшняя дата" есть в массиве 
+  puts "Сегодня (#{today}) день рождения у #{Petrovich(firstname: hbday[index + 1], lastname: hbday[index + 2]).to(:genitive).to_s}"
 end
 
